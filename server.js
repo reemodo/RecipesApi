4add494d6b8d7b5944eb37319e9fe4627c2ae57d
ReemSh <reem.shamasnah@gmail.com>
2024-01-09 13:34:59 +0200

    fix jquery

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 
 // Setting up express, serving client files, configuring bodyParser
-const port = 3000
+const port = 4000
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
